9b353c893c60c3b50f6210e9fb000346db307c92
spetseffect <spetseffect@gmail.com>
2021-03-19 16:12:24 +0200

    task4 decor

--- src/com/task4/Main.java
+++ src/com/task4/Main.java
@@ -19,9 +19,9 @@ public class Main {
         Predicate<Integer> moreZero = x -> x > 0;
         Predicate<Integer> lessZero = x -> x < 0;
         var arr = new Integer[]{7, -5, 8, -9, 2, -1, 5, -7, 8, -1, 3, -8, 9, 0, -8, 4, -6, 5, -7, 8, -6, 2, -3, 7, -4, 8, -8, 1, -7, 8, -1, 6, -7, 6, 0};
-        System.out.println(sum(arr, isEquals));
-        System.out.println(sum(arr, interval));
-        System.out.println(sum(arr, moreZero));
-        System.out.println(sum(arr, lessZero));
+        System.out.println(eq+" == x → "+sum(arr, isEquals));
+        System.out.println("a > x < b → "+sum(arr, interval));
+        System.out.println("x > 0 → "+sum(arr, moreZero));
+        System.out.println("x < 0 → "+sum(arr, lessZero));
     }
 }

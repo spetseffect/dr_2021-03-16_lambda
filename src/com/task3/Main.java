6b8ef2064afa762ba02a24d42afb459c741d8786
spetseffect <spetseffect@gmail.com>
2021-03-19 16:04:44 +0200

    task3 decor

--- src/com/task3/Main.java
+++ src/com/task3/Main.java
@@ -5,8 +5,8 @@ public class Main {
         var mm = new MinMax<Integer>(1, 2, 3, 4, 5);
         IMinMax<Integer> min = mm::searchMin;
         IMinMax<Integer> max = mm::searchMax;
-        System.out.println(min.search());
-        System.out.println(max.search());
+        System.out.println("Минимальное значение в массиве: "+min.search());
+        System.out.println("Максимальное значение в массиве: "+max.search());
     }
 
 }

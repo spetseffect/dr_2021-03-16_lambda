71b8308a683b539394e9740f7328499b7172db12
spetseffect <spetseffect@gmail.com>
2021-03-19 15:55:19 +0200

    task1 decor

--- src/com/task1/Main.java
+++ src/com/task1/Main.java
@@ -11,28 +11,29 @@ import java.util.function.Supplier;
 public class Main{
     public static void main(String[] args) throws ParseException {
         Predicate<Integer> year=(y)->y%4==0;
-        System.out.println(year.test(2020));
+        System.out.println("Вискосный год 2020: "+year.test(2020));
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date date1 = dateFormat.parse("09.01.1985");
-        Date date2 = dateFormat.parse("17.03.2021");
+        String dd1="09.01.1985",dd2="17.03.2021";
+        Date date1 = dateFormat.parse(dd1);
+        Date date2 = dateFormat.parse(dd2);
         IDateDiffDays ddd=(d1, d2)->{
             long ms=d2.getTime()-d1.getTime();
             return (int)(ms/1000/60/60/24);
         };
-        System.out.println(ddd.apply(date1,date2));
+        System.out.println("Количество дней между датами '"+dd1+"' и '"+dd2+"' = "+ddd.apply(date1,date2));
 
         IDateDiffWeeks ddw=(d1, d2)->{
             long ms=d2.getTime()-d1.getTime();
             return (int)(ms/1000/60/60/24/7);
         };
-        System.out.println(ddw.apply(date1,date2));
+        System.out.println("Количество полных недель между датами '"+dd1+"' и '"+dd2+"' = "+ddw.apply(date1,date2));
 
         LocalDate date = LocalDate.now();
         Supplier<String> wd=()->{
             DayOfWeek dayOfWeek = date.getDayOfWeek();
             return dayOfWeek.toString();
         };
-        System.out.println(wd.get());
+        System.out.println("День недели сегодня: "+wd.get());
     }
 }
